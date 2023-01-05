3b44ff81b720c947210778b9fca74be412b036e5
mwnakahara <85004753+mwnakahara@users.noreply.github.com>
2023-01-05 11:58:30 -0800

    Set up website

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>One Photo A Month</h1>
+      <h2>A photo journal about my little trilingual lion 🍁🦁🍙🥨</h2>
+
+      <p>
+        <em>
+          This page is still in development. Please check back in a few months.{" "}
+        </em>
+        😉
+      </p>
+
+      <p>
+        This page was coded by
+        <a href="https://gallant-colden-8ef37b.netlify.app" target="_blank">
+          {" "}
+          MWNakahara
+        </a>{" "}
+        and is
+        <a href="https://"> open-sourced</a>.
+      </p>
+    </div>
+  );
+}
+
+export default App;
